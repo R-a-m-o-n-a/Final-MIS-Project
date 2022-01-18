4cbab42b08b85b7a51d847d857731092946f9632
RobertoVendittiDesign <RobertoVendittiDesign@gmail.com>
2022-01-18 16:37:58 +0100

    Update TeensySketch.ino
    Changed threshold to try how bending works. With "10", it seems smoother

--- TeensySketch/TeensySketch.ino
+++ TeensySketch/TeensySketch.ino
@@ -9,7 +9,7 @@ uint16_t BNO055_SAMPLERATE_DELAY_MS = 10; //how often to read data from the boar
 uint16_t PRINT_DELAY_MS = 500; // how often to print the data
 uint16_t printCount = 0; //counter to avoid printing every 10MS sample
 
-const float THRESHOLD = 20; // threshold for move detection (when user bends left/right)
+const float THRESHOLD = 10; // threshold for move detection (when user bends left/right)
 
 // variables for 1st alternative of move detection (detection of the real peak of the movement)
 float prevAngle = 0;
